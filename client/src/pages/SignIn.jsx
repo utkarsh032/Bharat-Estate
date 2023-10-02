ba8043f104e737149045f4f5736fb79a4adfc3cd
utkarsh032 <utkarshhraj525@gmail.com>
2023-10-02 14:25:12 +0530

    create sign-in API route

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,11 @@
-
 export default function SignIn() {
   return (
     <div>
-      SignIn
+      <h1 className="text-3xl text-center font-semibold my-7">Sign in</h1>
+
+      <form>
+        <input />
+      </form>
     </div>
-  )
+  );
 }
