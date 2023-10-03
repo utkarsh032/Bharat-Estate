3dfac02634aca4d871b6a038f59d816c172524c8
utkarsh032 <utkarshhraj525@gmail.com>
2023-10-03 08:34:06 +0530

    Profile Page Private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -59,7 +59,7 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
-      res 
+      res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
         .json(rest);
