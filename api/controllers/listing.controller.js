3384ca004bf1d433ab0a9abf68ce0ce55e94d250
utkarsh032 <utkarshhraj525@gmail.com>
2023-10-15 08:10:35 +0530

    update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -26,4 +27,25 @@ export const deleteListing = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can update your own listing"));
+  }
+
+  try {
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateListing);
+  } catch (error) {
+    next(error);
+  } 
 };
