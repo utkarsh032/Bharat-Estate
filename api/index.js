1f74f947a6ed50dca00441f45621e20e2e38c03c
utkarsh032 <utkarshhraj525@gmail.com>
2023-10-19 19:28:20 +0530

    resolve api/index

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is running on PORT: 3000!");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
